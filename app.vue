286a6a384868e0fb9e22fa64a6d73fe864a3905a
Dev <redflame1028@gmail.com>
2023-03-21 16:13:56 -0400

    no message

---
+++ app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-col mt-6 ml-10">
+    <h1 class="text-3xl font-bold underline">
+      Experienced Vue3/Nuxt.js European Developer Needed
+    </h1>
+    <div class="mt-5">
+    <p class="text-xl font-bold">1.Include a link to your Github profile</p> 
+    <a class="p-2 cursor-pointer">https://github.com/perfectAlways1028</a>
+    </div>
+    <div class="mt-2">
+      <p class="text-xl font-bold">2. How many hours can you overlap in pacific timezone?</p> 
+      <p class="pt-2">I'm available on 7am ~ 2pm (PDT) 8hrs</p>
+    </div>
+    <div class="mt-2">
+      <p class="text-xl font-bold">3. How many hours are you available per week?</p> 
+      <p class="pt-2">40hrs</p>
+    </div>
+    <div class="mt-2">
+      <p class="text-xl font-bold">4. When can you start working?</p> 
+      <p class="pt-2">Immediately</p>
+    </div>
+        <div class="mt-2">
+      <p class="text-xl font-bold">5. What is your experience with Vue3/Nuxt? Have you ever worked as a lead dev?</p> 
+      <p class="pt-2">I have 2+ years experience in Vue3/Nuxt. I developed a school management site from scratch with vue3</p>
+    </div>
+  </div>
+</template>
